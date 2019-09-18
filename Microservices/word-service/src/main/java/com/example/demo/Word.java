d46ca11468c2113eb3df4e733efdf06b857127aa
b4ibmjava16@iiht.tech <b4ibmjava16@iiht.tech>
2019-09-18 22:19:05 +0530

    msg

---
+++ Microservices/word-service/src/main/java/com/example/demo/Word.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+public class Word {
+private String word;
+
+public Word() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+public Word(String word) {
+	super();
+	this.word = word;
+}
+
+public String getWord() {
+	return word;
+}
+
+public void setWord(String word) {
+	this.word = word;
+}
+
+
+
+}
